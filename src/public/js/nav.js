61cd02326384e86aa667fb8226782cfd4463fab9
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2025-02-24 16:18:17 +0200

    feat: implement burger menu functionality

--- src/public/js/nav.js
+++ src/public/js/nav.js
@@ -1,30 +1,79 @@
-const navIconElement = document.querySelector(".menu-icon-wrapper");
-const navElement = document.querySelector("nav");
+const navElement = document.querySelector(".nav");
+const logoNavWrapperElement = document.querySelector(".logo-nav-wrapper");
+const navIconsWrapperElement = document.querySelector(".menu-icon-wrapper");
+
+const navMobileContainer = document.querySelector("nav .nav-mobile");
+
 const navItemsContainerElement = document.querySelector(".nav-items-container");
 const navBtnsContainerElement = document.querySelector(".nav-btns-container");
 
+const registerBtnElement = document.querySelector(
+	".nav-btns-container .btn-primary"
+);
 const openIconElement = document.querySelector(".menu-icon-wrapper .fa-bars");
 const closeIconElement = document.querySelector(".menu-icon-wrapper .fa-x");
 
 const mobileScreensMediaQuery = window.matchMedia("(max-width: 860px)");
+
+const applyDesktopNav = () => {
+	navElement.appendChild(navBtnsContainerElement);
+	logoNavWrapperElement.appendChild(navItemsContainerElement);
+
+	navItemsContainerElement.classList.remove("flex-col-center");
+	navBtnsContainerElement.classList.remove(
+		"flex-col-center",
+		"gap-margin-top"
+	);
+
+	registerBtnElement.classList.remove("bg-unset");
+};
+
+const applyMobileNav = () => {
+	navMobileContainer.appendChild(navItemsContainerElement);
+	navMobileContainer.appendChild(navBtnsContainerElement);
+	navMobileContainer.classList.add("nav-hidden");
+
+	navItemsContainerElement.classList.add("flex-col-center");
+	navBtnsContainerElement.classList.add("flex-col-center", "gap-margin-top");
+
+	registerBtnElement.classList.add("bg-unset");
+};
+
 const mediaScreenResizeHandler = (e) => {
-    // change burger menu's close icon to open icon when media screen changes
+	// change burger menu's close icon to open icon when media screen changes
 	if (closeIconElement.classList.contains("open-menu")) {
 		closeIconElement.classList.replace("open-menu", "close-menu");
 		openIconElement.classList.replace("close-menu", "open-menu");
+	}
+
+	// goes from desktop to mobile
+	if (e.matches == true) {
+		applyMobileNav();
+	} else {
+		// goes from mobile to desktop
+		applyDesktopNav();
 	}
 };
 
 const navIconClickHandler = () => {
 	// Change burger menu icon on click
 	if (openIconElement.classList.contains("open-menu")) {
+		// case when menu is closed
 		openIconElement.classList.replace("open-menu", "close-menu");
 		closeIconElement.classList.replace("close-menu", "open-menu");
+		navMobileContainer.classList.remove("nav-hidden");
 	} else {
+		// case when menu is opened
 		openIconElement.classList.replace("close-menu", "open-menu");
 		closeIconElement.classList.replace("open-menu", "close-menu");
+		navMobileContainer.classList.add("nav-hidden");
 	}
 };
 
 mobileScreensMediaQuery.addEventListener("change", mediaScreenResizeHandler);
-navIconElement.addEventListener("click", navIconClickHandler);
+navIconsWrapperElement.addEventListener("click", navIconClickHandler);
+window.onload = function () {
+	if (window.innerWidth < 860) {
+		applyMobileNav();
+	}
+};
